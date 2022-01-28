900d352999a30c0128bcd23559acbe6922884cba
Axel LUKONGO <alukongo@e1r11p1.42.fr>
2022-01-28 14:42:46 +0100

    28/01/2022 14:42

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,21 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 16:19:07 by alukongo          #+#    #+#             */
-/*   Updated: 2022/01/22 18:47:42 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/01/28 14:42:20 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/*
+here i check if my line exist else i return null.
+line 32) this condition is only for don't have a negative value in size_line
+line 35) i copy the char from line to 0 until the \n to the str
+line 36) i copy the char from line to size_line in line, and i free the old address of line
+*/
 char	*get_the_line(char **line, int size_line)
 {
-	char	*s;
+	char	*str;
 
 	if (!*line || !**line)
 	{
@@ -27,11 +33,17 @@ char	*get_the_line(char **line, int size_line)
 	}
 	if (size_line == -1)
 		size_line = ft_strlen(*line);
-	s = ft_substr(*line, 0, size_line + 1, 0);
+	str = ft_substr(*line, 0, size_line + 1, 0);
 	*line = ft_substr(*line, size_line + 1, ft_strlen(*line), 1);
-	return (s);
+	return (str);
 }
 
+/*
+first i check of the fd, BUFFER_SIZE, and read
+line 46) i check the number of char before the \n in my line otherwise line = -1 and i read.
+in my loop while there isn't \n in line and ret > 0, i joint the buffer in my line and i read again.
+finaly if i find a \n or my ret < 0 i exit my loop and i return get_the line.
+*/
 char	*get_next_line(int fd)
 {
 	char		buf[BUFFER_SIZE];
